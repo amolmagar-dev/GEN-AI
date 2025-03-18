af81b6b26e95d373fece2c48ccf716020cfe0df1
Amol Magar <amolmagar.dev@gmail.com>
2025-03-18 09:31:21 +0530

    Remove deprecated function declarations and browser helper functions

--- Laborers/declarations.js
+++
@@ -1,5 +0,0 @@
-import { functionDeclarations } from "./functions/browserHelper.js";
-import { controlLightFunctionDeclaration } from "./functions/controlLight.js";
-import { googleSearchFunctionDeclaration } from "./functions/searchEngine.js";
-
-export const declarations = [controlLightFunctionDeclaration, googleSearchFunctionDeclaration, functionDeclarations]
